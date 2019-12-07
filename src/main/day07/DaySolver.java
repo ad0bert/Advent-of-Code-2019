ebf6622d2b1798b7993aacc10e0936b874553b18
ad0bert <pe.ortner@gmail.com>
2019-12-07 20:48:12 +0100

    add day7 solution

--- src/main/day07/DaySolver.java
+++ src/main/day07/DaySolver.java
@@ -1,27 +1,112 @@
 package main.day07;
 
 import java.io.File;
-import java.util.List;
+import java.util.*;
 
 import main.AbstractSolver;
 import utils.AoCFileReader;
+import utils.IntCode;
 
 public class DaySolver extends AbstractSolver {
+
+    private Map<Integer, Map<Integer, Integer>> resultMap;
+
+    private List<int[]> configs;
 
     public DaySolver(String day) {
         super(day);
     }
 
+    private void init(int[] configSettings) {
+        resultMap = new HashMap<>();
+        resultMap.put(0, new HashMap<>());
+        resultMap.put(1, new HashMap<>());
+        resultMap.put(2, new HashMap<>());
+        resultMap.put(3, new HashMap<>());
+        resultMap.put(4, new HashMap<>());
+        configs = new ArrayList<>();
+        this.calcConfig(configSettings.length, configSettings);
+    }
+
     @Override
     public void solvePart1() {
-        List<List<String>> input = AoCFileReader.readListOfCharList(new File(this.inputFile1));
-        System.out.println();
+        List<Integer> input = AoCFileReader.readIntegerLine(new File(this.inputFile1));
+        IntCode program = new IntCode(input);
+        int[] phaseSettings = {0, 1, 2, 3, 4};
+        this.init(phaseSettings);
+
+        List<Integer> outputs = new ArrayList<>();
+        for (int[] config : this.configs) {
+            int res = 0;
+            for (int i : config) {
+                int[] args = {i, res};
+                if (resultMap.get(args[0]) != null && resultMap.get(args[0]).get(args[1]) != null) {
+                    res = resultMap.get(args[0]).get(args[1]);
+                } else {
+                    res = program.run(args);
+                    resultMap.get(args[0]).put(args[1], res);
+                }
+            }
+            outputs.add(res);
+        }
+        System.out.println(Collections.max(outputs));
     }
 
     @Override
     public void solvePart2() {
-        List<List<String>> input = AoCFileReader.readListOfCharList(new File(this.inputFile2));
-        System.out.println();
+        List<Integer> input = AoCFileReader.readIntegerLine(new File(this.inputFile2));
+
+
+        int[] phaseSettings = {5, 6, 7, 8, 9};
+        this.init(phaseSettings);
+
+        List<Integer> outputs = new ArrayList<>();
+        for (int[] config : this.configs) {
+            IntCode[] amps = {
+                    new IntCode(input),
+                    new IntCode(input),
+                    new IntCode(input),
+                    new IntCode(input),
+                    new IntCode(input)
+            };
+            // init with config
+            for (int i = 0; i < amps.length; i++) {
+                amps[i].code = config[i];
+            }
+            int res = 0;
+            int curr = 0;
+            while (Arrays.stream(amps).anyMatch(amp -> amp.running)) {
+                curr = curr % 5;
+                res = amps[curr].runPersist(res);
+                if (curr == 4) {
+                    outputs.add(res);
+                }
+                curr++;
+            }
+        }
+        System.out.println(Collections.max(outputs));
+    }
+
+    public void calcConfig(int pos, int[] elements) {
+        if (pos == 1) {
+            this.configs.add(Arrays.copyOf(elements, 5));
+        } else {
+            for (int i = 0; i < pos - 1; i++) {
+                calcConfig(pos - 1, elements);
+                if (pos % 2 == 0) {
+                    swap(elements, i, pos - 1);
+                } else {
+                    swap(elements, 0, pos - 1);
+                }
+            }
+            calcConfig(pos - 1, elements);
+        }
+    }
+
+    private void swap(int[] toSwap, int a, int b) {
+        int tmp = toSwap[a];
+        toSwap[a] = toSwap[b];
+        toSwap[b] = tmp;
     }
 
 }
---
+++ src/utils/IntCode.java
@@ -0,0 +1,160 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntCode {
+
+    private final Integer ADD = 1;
+    private final Integer MUL = 2;
+    private final Integer READ = 3;
+    private final Integer WRITE = 4;
+    private final Integer JIT = 5;
+    private final Integer JIF = 6;
+    private final Integer LT = 7;
+    private final Integer EQ = 8;
+    private final Integer HALT = 99;
+
+    private List<Integer> program;
+
+    public IntCode(List<Integer> program) {
+        this.program = new ArrayList<>(program);
+    }
+
+    public int run(int[] args) {
+        int instructionCnt;
+        int argument = 0;
+        List<Integer> outputBuffer = new ArrayList<>();
+        List<Integer> input = new ArrayList<>(this.program);
+        int[] instruction;
+        for (int i = 0; i < input.size(); i += instructionCnt) {
+            instruction = String.format("%05d", input.get(i)).chars().map(c -> c - '0').toArray();
+            int op = Integer.parseInt(String.valueOf(instruction[3]) + String.valueOf(instruction[4]));
+            if (ADD.equals(op)) {
+                int val1 = instruction[2] == 0 ? input.get(input.get(i + 1)) : input.get(i + 1);
+                int val2 = instruction[1] == 0 ? input.get(input.get(i + 2)) : input.get(i + 2);
+                input.set(input.get(i + 3), val1 + val2);
+                instructionCnt = 4;
+            } else if (MUL.equals(op)) {
+                int val1 = instruction[2] == 0 ? input.get(input.get(i + 1)) : input.get(i + 1);
+                int val2 = instruction[1] == 0 ? input.get(input.get(i + 2)) : input.get(i + 2);
+                input.set(input.get(i + 3), val1 * val2);
+                instructionCnt = 4;
+            } else if (READ.equals(op)) {
+                input.set(input.get(i + 1), args[argument++]);
+                instructionCnt = 2;
+            } else if (WRITE.equals(op)) {
+                outputBuffer.add(instruction[2] == 0 ? input.get(input.get(i + 1)) : input.get(i + 1));
+                instructionCnt = 2;
+            } else if (JIT.equals(op)) {
+                int val1 = instruction[2] == 0 ? input.get(input.get(i + 1)) : input.get(i + 1);
+                int val2 = instruction[1] == 0 ? input.get(input.get(i + 2)) : input.get(i + 2);
+                if (val1 != 0) {
+                    i = val2;
+                    instructionCnt = 0;
+                } else {
+                    instructionCnt = 3;
+                }
+            } else if (JIF.equals(op)) {
+                int val1 = instruction[2] == 0 ? input.get(input.get(i + 1)) : input.get(i + 1);
+                int val2 = instruction[1] == 0 ? input.get(input.get(i + 2)) : input.get(i + 2);
+                if (val1 == 0) {
+                    i = val2;
+                    instructionCnt = 0;
+                } else {
+                    instructionCnt = 3;
+                }
+            } else if (LT.equals(op)) {
+                int val1 = instruction[2] == 0 ? input.get(input.get(i + 1)) : input.get(i + 1);
+                int val2 = instruction[1] == 0 ? input.get(input.get(i + 2)) : input.get(i + 2);
+                input.set(input.get(i + 3), val1 < val2 ? 1 : 0);
+                instructionCnt = 4;
+            } else if (EQ.equals(op)) {
+                int val1 = instruction[2] == 0 ? input.get(input.get(i + 1)) : input.get(i + 1);
+                int val2 = instruction[1] == 0 ? input.get(input.get(i + 2)) : input.get(i + 2);
+                input.set(input.get(i + 3), val1 == val2 ? 1 : 0);
+                instructionCnt = 4;
+            } else if (HALT.equals(op)) {
+                break;
+            } else {
+                System.out.println("BEEP");
+                break;
+            }
+
+        }
+        return outputBuffer.get(outputBuffer.size() - 1);
+    }
+
+    private int ip = 0;
+    public boolean running = true;
+    private boolean init = false;
+    public int code = 0;
+
+    public int runPersist(int param) {
+        int instructionCnt;
+        int[] instruction;
+        for (int i = ip; i < program.size(); i += instructionCnt) {
+            instruction = String.format("%05d", program.get(i)).chars().map(c -> c - '0').toArray();
+            int op = Integer.parseInt(String.valueOf(instruction[3]) + String.valueOf(instruction[4]));
+            if (ADD.equals(op)) {
+                int val1 = instruction[2] == 0 ? program.get(program.get(i + 1)) : program.get(i + 1);
+                int val2 = instruction[1] == 0 ? program.get(program.get(i + 2)) : program.get(i + 2);
+                program.set(program.get(i + 3), val1 + val2);
+                instructionCnt = 4;
+            } else if (MUL.equals(op)) {
+                int val1 = instruction[2] == 0 ? program.get(program.get(i + 1)) : program.get(i + 1);
+                int val2 = instruction[1] == 0 ? program.get(program.get(i + 2)) : program.get(i + 2);
+                program.set(program.get(i + 3), val1 * val2);
+                instructionCnt = 4;
+            } else if (READ.equals(op)) {
+                if (!init) {
+                    program.set(program.get(i + 1), code);
+                    init = true;
+                } else {
+                    program.set(program.get(i + 1), param);
+                }
+                instructionCnt = 2;
+            } else if (WRITE.equals(op)) {
+                int out = (instruction[2] == 0 ? program.get(program.get(i + 1)) : program.get(i + 1));
+//                instructionCnt = 2;
+                ip = i + 2;
+                return out;
+            } else if (JIT.equals(op)) {
+                int val1 = instruction[2] == 0 ? program.get(program.get(i + 1)) : program.get(i + 1);
+                int val2 = instruction[1] == 0 ? program.get(program.get(i + 2)) : program.get(i + 2);
+                if (val1 != 0) {
+                    i = val2;
+                    instructionCnt = 0;
+                } else {
+                    instructionCnt = 3;
+                }
+            } else if (JIF.equals(op)) {
+                int val1 = instruction[2] == 0 ? program.get(program.get(i + 1)) : program.get(i + 1);
+                int val2 = instruction[1] == 0 ? program.get(program.get(i + 2)) : program.get(i + 2);
+                if (val1 == 0) {
+                    i = val2;
+                    instructionCnt = 0;
+                } else {
+                    instructionCnt = 3;
+                }
+            } else if (LT.equals(op)) {
+                int val1 = instruction[2] == 0 ? program.get(program.get(i + 1)) : program.get(i + 1);
+                int val2 = instruction[1] == 0 ? program.get(program.get(i + 2)) : program.get(i + 2);
+                program.set(program.get(i + 3), val1 < val2 ? 1 : 0);
+                instructionCnt = 4;
+            } else if (EQ.equals(op)) {
+                int val1 = instruction[2] == 0 ? program.get(program.get(i + 1)) : program.get(i + 1);
+                int val2 = instruction[1] == 0 ? program.get(program.get(i + 2)) : program.get(i + 2);
+                program.set(program.get(i + 3), val1 == val2 ? 1 : 0);
+                instructionCnt = 4;
+            } else if (HALT.equals(op)) {
+                break;
+            } else {
+                System.out.println("BEEP");
+                break;
+            }
+        }
+        running = false;
+        return -1;
+    }
+}
